41ee6765004cd6f88effdcdb5f798f18ed74dc83
Eddie Antonio Santos <easantos@ualberta.ca>
2018-01-31 10:42:11 -0700

    Create test to decode alphanumerics (identity function).

--- pctncd_test.py
+++ pctncd_test.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from pctncd import encode, decode
+from pctncd import decode
+from string import ascii_letters, digits
+
+from hypothesis import given
+from hypothesis.strategies import text
 
 
-def test_identity():
-    ...
+ascii_alphanumerics = ascii_letters + digits
+
+
+@given(text(alphabet=ascii_alphanumerics))
+def test_identity_for_alphanumerics(s):
+    assert s == decode(s)
